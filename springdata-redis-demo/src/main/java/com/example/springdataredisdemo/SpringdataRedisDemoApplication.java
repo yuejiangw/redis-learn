1bb5a0277c6b35debe52234d66fc96060190f956
226wyj <308033240@qq.com>
2023-12-02 12:06:02 -0800

    first commit

---
+++ springdata-redis-demo/src/main/java/com/example/springdataredisdemo/SpringdataRedisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdataredisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataRedisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdataRedisDemoApplication.class, args);
+    }
+
+}
---
+++ springdata-redis-demo/src/test/java/com/example/springdataredisdemo/SpringdataRedisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdataredisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataRedisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
