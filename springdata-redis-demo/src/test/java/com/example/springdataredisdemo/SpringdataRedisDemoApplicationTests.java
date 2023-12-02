1c39b7d04a3a3fa85de0a3282acadf6c6ee090b6
226wyj <308033240@qq.com>
2023-12-02 12:45:41 -0800

    init

--- springdata-redis-demo/src/test/java/com/example/springdataredisdemo/SpringdataRedisDemoApplicationTests.java
+++ springdata-redis-demo/src/test/java/com/example/springdataredisdemo/SpringdataRedisDemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.springdataredisdemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class SpringdataRedisDemoApplicationTests {
 
+    private final StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    SpringdataRedisDemoApplicationTests(final StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
     @Test
-    void contextLoads() {
+    void testString() {
+        stringRedisTemplate.opsForValue().set("name", "zhangsan");
+        System.out.println(stringRedisTemplate.opsForValue().get("name"));
     }
 
 }
