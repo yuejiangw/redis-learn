2fc035770af89047fdb0e21f557ec1ccfb137049
Yuejiang Wu <yuejiangw@indeed.com>
2023-12-10 17:47:31 -0800

    prevent cache penetration by cache null value

--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -12,7 +12,11 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_NULL_TTL;
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 
 /**
  * <p>
@@ -29,23 +33,49 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Result queryById(Long id) {
+        final String key = CACHE_SHOP_KEY + id;
         // 1. 从 Redis 查询商铺缓存
-        String shopJson = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id); // 得到 JSON 字符串
+        String shopJson = stringRedisTemplate.opsForValue().get(key); // 得到 JSON 字符串
         // 2. 判断是否存在
         if (StrUtil.isNotBlank(shopJson)) {
             // 3. 如果存在，直接返回
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
+        // 判断命中的是否是空值
+        if (shopJson != null) {
+            // 返回一个错误信息
+            return Result.fail("店铺信息不存在");
+        }
+
         // 4. 如果不存在，根据 id 查询数据库
         Shop shop = getById(id);
         // 5. 如果数据库不存在，返回错误信息
         if (shop == null) {
+            // 防止缓存穿透，缓存空值
+            stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
             return Result.fail("商铺不存在");
         }
         // 6. 如果数据库存在，写入 Redis
-        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop));
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
         // 7. 返回数据
         return null;
+    }
+
+    @Override
+    public Result update(final Shop shop) {
+        Long id = shop.getId();
+        if (id == null) {
+            return Result.fail("店铺 id 不能为空");
+        }
+
+        // 1. 更新数据库
+        updateById(shop);
+
+        // 2. 删除缓存
+        // p.s. 由于是单体项目，因此我们的数据库更新和缓存删除都在一起进行，如果是分布式项目，缓存删除可能是由其他项目来完成
+        // 这时候要通过 MQ 来通知别的 service
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + id);
+        return Result.ok();
     }
 }
