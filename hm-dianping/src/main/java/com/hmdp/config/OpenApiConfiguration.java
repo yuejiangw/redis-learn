324f499aaaa9bd08672c10cfb8796265cd4d4a83
226wyj <308033240@qq.com>
2024-11-07 22:55:19 -0800

    update db config

--- hm-dianping/src/main/java/com/hmdp/config/OpenApiConfiguration.java
+++ hm-dianping/src/main/java/com/hmdp/config/OpenApiConfiguration.java
@@ -1,47 +1,24 @@
 package com.hmdp.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.RequestHandler;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.function.Predicate;
-
-
-@EnableOpenApi
 @Configuration
 public class OpenApiConfiguration {
-
     @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.OAS_30)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(apis())
-                .paths(PathSelectors.any())
-                .build()
-                .enable(true);
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("标题")
+                        .description("我的API文档")
+                        .version("v1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("外部文档")
+                        .url("https://springshop.wiki.github.org/docs"));
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("项目名称")
-                .description("项目描述")
-                .termsOfServiceUrl("项目地址")
-                .version("API版本")
-                .license("公司的license")
-                .licenseUrl("公司的license地址")
-                .build();
-    }
-
-    private Predicate<RequestHandler> apis() {
-        return RequestHandlerSelectors.basePackage("com.hmdp.controller");
-    }
 }
-
